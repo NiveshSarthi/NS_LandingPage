e6124ba3478acd867c13dd23b2d3bc568c89434d
Ratnaker56 <ratnakerkumar56@gmail.com>
2026-02-10 17:35:51 +0530

    style: significantly increase logo size for better visibility

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export function Logo({ className }: { className?: string }) {
     return (
         <Link href="/" className={`block relative ${className}`}>
-            <div className="relative h-16 w-56 md:h-20 md:w-64">
+            <div className="relative h-32 w-80 md:h-40 md:w-96">
                 <Image
                     src="/logo.png"
                     alt="NiveshSarthi Logo"
