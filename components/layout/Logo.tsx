c80032a74aae00d2e7a154f4e3003294f04294a8
Ratnaker56 <ratnakerkumar56@gmail.com>
2026-02-10 17:33:17 +0530

    style: increase application logo size for better visibility

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export function Logo({ className }: { className?: string }) {
     return (
         <Link href="/" className={`block relative ${className}`}>
-            <div className="relative h-12 w-40 md:h-14 md:w-48">
+            <div className="relative h-16 w-56 md:h-20 md:w-64">
                 <Image
                     src="/logo.png"
                     alt="NiveshSarthi Logo"
